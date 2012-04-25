1adc999a3bfbb274008d99459786090d30adf1df
guanlun <guanlunzhao@gmail.com>
2012-04-25 16:48:14 +0800

    particle system completed

--- AniSkelS12/particleSystem.cpp
+++ AniSkelS12/particleSystem.cpp
@@ -19,15 +19,10 @@ ParticleSystem::ParticleSystem()
 {
 	srand(time(0));
 
-	Gravity* g = new Gravity(Vec3<float>(0, -0.001, 0));
-	Friction* f = new Friction(0.01);
-
-	forces.push_back(g);
-	forces.push_back(f);
+	forces.push_back(new Gravity(Vec3<float>(0, -0.02, 0)));
+	forces.push_back(new Friction(0.001));
+	forces.push_back(new Disturbance(0.1));
 }
-
-
-
 
 /*************
  * Destructor
@@ -49,6 +44,7 @@ void ParticleSystem::startSimulation(float t)
 {
     
 	// TODO
+
 
 	// These values are used by the UI ...
 	// -ve bake_end_time indicates that simulation
@@ -89,59 +85,72 @@ void ParticleSystem::resetSimulation(float t)
 /** Compute forces and update particles **/
 void ParticleSystem::computeForcesAndUpdateParticles(float t)
 {
+	bake_fps = t - curr_time;
+	curr_time = t;
 	if (simulate) {
-		vector<Particle*>::iterator iter;
-		for (iter = particles.begin(); iter != particles.end(); iter++) {
-			(*iter)->move();
+		if (!isBakedAt(t)) {
+			vector<Particle>::iterator iter;
+			for (iter = particles.begin(); iter != particles.end(); iter++) {
+				iter->move(bake_fps);
+			}
+			bakeParticles(t);
+		} else {
+			particles = bakeData[t];
 		}
 	}
 }
-
 
 /** Render particles */
 void ParticleSystem::drawParticles(float t)
 {
 	if (simulate) {
-		vector<Particle*>::iterator iter;
-		for (iter = particles.begin(); iter != particles.end(); iter++) {
-			(*iter)->draw();
+		vector<Particle>::iterator iter;
+		for (iter = bakeData[t].begin(); iter != bakeData[t].end(); iter++) {
+			iter->draw();
 		}
 	}
 }
-
-
-
-
 
 /** Adds the current configuration of particles to
   * your data structure for storing baked particles **/
 void ParticleSystem::bakeParticles(float t) 
 {
-
-	// TODO
+	bakeData.insert(pair<float, vector<Particle>>(t, particles));
 }
 
 /** Clears out your data structure of baked particles */
 void ParticleSystem::clearBaked()
 {
-
-	// TODO
+	bakeData.clear();
 }
 
 void ParticleSystem::addParticlesAt(Vec3<float> pos, int num) {
-	for (int i = 0; i < num; i++) {
-		Particle* p = new Particle(Vec3<float>(pos[0], pos[1], pos[2]), 1);
-	
-		float xSpeed = rand() % 10 / 100.0 + 0.1;
-		float zSpeed = rand() % 10 / 100.0 - 0.05;
-		float ySpeed = rand() % 10 / 100.0 - 0.05;
-		p->setSpeed(Vec3<float>(xSpeed, ySpeed, zSpeed));
-	
-		vector<Force*>::iterator iter;
-		for (iter = forces.begin(); iter != forces.end(); iter++) {
-			p->addForce(*iter);
+	if (simulate) {
+		if (!isBakedAt(curr_time + bake_fps)) {
+			for (int i = 0; i < num; i++) {
+				Particle p(Vec3<float>(pos[0], pos[1], pos[2]), 1);
+			
+				float mag = rand() % 10 / 10.0 + 0.2;
+				float theta = rand() % 360 / 57.3;
+		
+				float xSpeed = rand() % 10 / 10.0 + 2;
+				float ySpeed = cos(theta) * mag;
+				float zSpeed = sin(theta) * mag;
+		
+				p.setSpeed(Vec3<float>(xSpeed, ySpeed, zSpeed));
+			
+				vector<Force*>::iterator iter;
+				for (iter = forces.begin(); iter != forces.end(); iter++) {
+					p.addForce(*iter);
+				}
+			
+				particles.push_back(p);
+			}
 		}
-	
-		particles.push_back(p);
 	}
+}
+
+bool ParticleSystem::isBakedAt(float t) {
+	map<float, vector<Particle>>::iterator it = bakeData.find(t);
+	return (it != bakeData.end());
 }
